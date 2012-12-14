a4d3707c5d054dc1c911f2d7ed046a3a9bcebcbe
Joe Nelson <cred+github@begriffs.com>
2012-12-13 23:15:08 -0600

    Fix Heroku asset precompile

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Csstrashman
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't connect to DB during asset precompile (fixes Heroku)
+    config.assets.initialize_on_precompile = false
   end
 end
